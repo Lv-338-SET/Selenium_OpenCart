6e244edfcd97c8b41a2c6a1218d15d38194eae7c
Orest Syniavskyi <orestsyn@gmail.com>
2018-10-05 18:04:32 +0300

    Me and Volodya

---
+++ Selenium_OpenCart/Tests/WishListTest.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using NUnit.Framework;
+using TestSite.Pages;
+
+
+namespace OpenCart
+{
+    
+    [TestFixture]
+    public class OpenCartTest
+    {
+
+        IWebDriver driver;
+
+        [TestCase("iPhone")]
+        [Order(0)]
+        public void WishListWorks_AddingIphone_IsAdded(string product)
+        {
+            
+            //PRODUCT SEARCHING
+            driver.FindElement(By.XPath("//input[@type= 'text' and @name= 'search']")).SendKeys(product);
+            driver.FindElement(By.XPath("//*[@id='search']/span/button")).Click();
+            IJavaScriptExecutor js = driver as IJavaScriptExecutor;
+            js.ExecuteScript("window.scrollBy(0,0);");
+            driver.FindElement(By.XPath("//a[text()='" + product + "']/../../../..//i[@class='fa fa-heart']")).Click();
+            WishListPage wishList = new WishListPage(driver);
+            wishList.ClickWishListButton();
+            bool result = wishList.IsProductAdded(product);
+            Assert.IsTrue(result, "Expected element is added to wishlist");
+            
+        }
+
+
+        [TestCase("iPhone")]
+        [Order(1)]
+
+        public void AddToCartFromWishList_AddIphone_IsAdded(string product)
+        {
+            WishListPage wishList = new WishListPage(driver);
+            
+        }
+
+
+        [TestCase("iPhone")]
+        [Order(2)]
+
+        public void RemoveFromWishList_RemoveIphone_IsRemoved(string product)
+        {
+            
+        }
+
+        [OneTimeTearDown]
+        public void AfterClass()
+        {
+            driver.Quit();
+            
+        }
+
+        [OneTimeSetUp]
+        public void BeforeClass()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
+            driver.Navigate().GoToUrl("http://atqc-shop.epizy.com/");
+
+            //Login page opening
+            driver.FindElement(By.ClassName("caret")).Click();
+            driver.FindElement(By.LinkText("Login")).Click();
+
+            //LOGGING IN
+            driver.FindElement(By.Id("input-email")).SendKeys("iwilltestyounow@gmail.com");
+            driver.FindElement(By.Id("input-password")).SendKeys("qwerty12345");
+            driver.FindElement(By.XPath("//input[@type='submit' and @value='Login']")).Click();
+        }
+
+    }
+}
