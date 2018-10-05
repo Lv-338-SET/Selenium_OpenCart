e911b30f7f5dee4080219effca43cf5532e3623e
Andrii Turkevych <andriiturkevych@gmail.com>
2018-10-06 00:52:58 +0300

    Fixed Class topbar and added some emty classes

---
+++ Selenium_OpenCart/Pages/Body/CartPage/WishListPage.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+
+namespace Selenium_OpenCart.Pages.Body.CartPage
+{
+     public class WishListPage
+    {
+
+        protected IWebElement WishListTable { get; private set; }
+        protected IWebElement AddToCartButton { get; private set; }
+        protected IWebElement RemoveFromWishlistButton { get; private set; }
+        
+        public WishListPage(IWebDriver driver)
+        {
+            WishListTable = driver.FindElement(By.CssSelector(".table-responsive"));
+
+        }
+
+        public void AddToCartButtonClick(IWebDriver driver,string product)
+        {
+
+        }
+
+        public void RemoveProductFromWishListClick(IWebDriver driver,string product)
+        {
+           driver.FindElement(By.XPath("//a[text()='" + product + "']//..//..//*[@data-original-title='Remove']")).Click();
+        }
+
+        public IWebElement GetTable()
+        {
+            return this.WishListTable;
+        }
+        
+        
+    }
+}
---
+++ Selenium_OpenCart/Pages/Header/MyAccount.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace Selenium_OpenCart.Pages.Header
+{
+    public class MyAccount
+    {
+        private static MyAccount Account = null;
+        //private MyAccount() { }
+
+        public static MyAccount MyAccountMenu(IWebDriver driver)
+        {
+            if (IsLogined(driver))
+            {
+                Account = new LoginAcountElements(driver);
+            }
+            else
+            {
+                Account = new NotLoginetAcountElements(driver);
+            }
+            return Account;
+        }
+
+        private static bool IsLogined(IWebDriver driver)
+        {
+            return driver.FindElement(By.XPath("//a[text()='Register']")).Displayed;
+        }
+    }
+
+    public class LoginAcountElements: MyAccount
+    {
+        private IWebDriver driver;
+
+        public IWebElement Register
+        {
+            get
+            {
+                return driver.FindElement(By.XPath("//a[text()='Register']"));
+            }
+        }
+
+        public IWebElement Login
+        {
+            get
+            {
+                return driver.FindElement(By.XPath("//a[text()='Login']"));
+            }
+        }
+
+        public LoginAcountElements(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+    }
+
+    public class NotLoginetAcountElements: MyAccount
+    {
+        private IWebDriver driver;
+
+        public IWebElement MyAccount
+        {
+            get
+            {
+                return driver.FindElement(By.XPath("//a[text()='My Account']"));
+            }
+        }
+        public IWebElement OrderHistory
+        {
+            get
+            {
+                return driver.FindElement(By.XPath("//a[text()='Order History']"));
+            }
+        }
+        public IWebElement Transaction
+        {
+            get
+            {
+                return driver.FindElement(By.XPath("//a[text()='Transactions']"));
+            }
+        }
+        public IWebElement Downloads
+        {
+            get
+            {
+                return driver.FindElement(By.XPath("//a[text()='Downloads']"));
+            }
+        }
+        public IWebElement Logout
+        {
+            get
+            {
+                return driver.FindElement(By.XPath("//a[text()='Logout']"));
+            }
+        }
+
+        public NotLoginetAcountElements(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+    }
+}
