8ea059debbd09deb8a4104a485eab8e67911f6a4
Nazar <nazar2131@ukr.net>
2018-10-06 14:44:52 +0300

    Changed home page and added one more class

---
+++ Selenium_OpenCart/Logic/AddToCartMetods.cs
@@ -0,0 +1,26 @@
+﻿using OpenQA.Selenium;
+using Selenium_OpenCart.Pages.Body.MainPage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Selenium_OpenCart.Logic
+{
+    class AddToCartMetods
+    {
+        IWebDriver webDriver;
+        public void n()
+        {
+            HomePage homePage = new HomePage(webDriver);
+            homePage.GetListProduct();
+            foreach (var s in homePage.GetListProduct())
+            {
+                s.ClickCartButton();
+            }
+        }
+       
+
+    }
+}
