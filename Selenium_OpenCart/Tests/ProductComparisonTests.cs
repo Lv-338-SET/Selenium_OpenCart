8f0c0ced63e4ebc856d4bc50007a501b98b2f5c9
Андрій Копитко <yovjik89@gmail.com>
2018-10-08 10:27:33 +0300

    Edit ProductComparisonTests

--- Selenium_OpenCart/Tests/ProductComparisonTests.cs
+++ Selenium_OpenCart/Tests/ProductComparisonTests.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +8,10 @@ using System.Threading.Tasks;
 
 namespace Selenium_OpenCart.Tests
 {
+    [TestFixture]
     public class ProductComparisonTests
     {
+        //IWebDriver driver;
 
     }
 }
