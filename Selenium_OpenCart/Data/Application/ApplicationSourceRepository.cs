25b08f7c618099576feef6d3566f80d51f9464b4
Andrii Turkevych <andriiturkevych@gmail.com>
2018-10-08 18:54:35 +0300

    Added Engine and SearchBy

---
+++ Selenium_OpenCart/Data/Application/ApplicationSourceRepository.cs
@@ -0,0 +1,47 @@
+﻿namespace Selenium_OpenCart.Data.Application
+{
+    public sealed class ApplicationSourceRepository
+    {
+        public const string FIREFOX_BROWSER = "Firefox";
+        public const string CHROME_BROWSER = "Chrome";
+        public const string INTERNET_EXPLORER_BROWSER = "InternetExplore";
+
+        private ApplicationSourceRepository() { }
+
+        public static ApplicationSource Default()
+        {
+            return ChromeTAQC();
+        }
+
+        public static ApplicationSource ChromeTAQC()
+        {
+            var option = new[] { "--no-proxy-server", "--ignore-certificate-errors" 
+                        , "--disable-extensions", "--start-maximized", "--ignore-certificate-errors" };
+            return new ApplicationSource(CHROME_BROWSER, 10L, 10L,
+                "http://atqc-shop.epizy.com/", option);
+        }
+
+        public static ApplicationSource ChromeNew()
+        {
+            return new ApplicationSource(CHROME_BROWSER, 10L, 10L,
+                "http://40.118.125.245/");
+        }
+        public static ApplicationSource ChromeDemo()
+        {
+            return new ApplicationSource(CHROME_BROWSER, 10L, 10L,
+                "https://demo.opencart.com/");
+        }
+
+        public static ApplicationSource InternetExplorerDemo()
+        {
+            return new ApplicationSource(INTERNET_EXPLORER_BROWSER, 10L, 10L,
+                "http://regres.herokuapp.com/login");
+        }
+
+        public static ApplicationSource FirefoxDemo()
+        {
+            return new ApplicationSource(FIREFOX_BROWSER, 10L, 10L,
+                "http://regres.herokuapp.com/login");
+        }
+    }
+}
