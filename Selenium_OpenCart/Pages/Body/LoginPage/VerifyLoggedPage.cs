b715f2cfecdbd2585e9b1d01dc58c9c828962cf0
orest77 <orest.shkhumov@gmail.com>
2018-10-10 16:38:32 +0300

    Add new

---
+++ Selenium_OpenCart/Pages/Body/LoginPage/VerifyLoggedPage.cs
@@ -0,0 +1,27 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Selenium_OpenCart.Pages.Body.LoginPage
+{
+    public class VerifyLoggedPage
+    {
+        public static bool VerifyLoggedUser(IWebDriver driver)
+        {
+
+            try
+            {
+                driver.FindElement(By.XPath("//a[contains(@href,'/logout')]"));
+                return true;
+            }
+            catch (NoSuchElementException)
+            {
+                return false;
+            }
+
+        }
+    }
+}
--- Selenium_OpenCart/Pages/Body/RegisterPage/RegisterPage.cs
+++ Selenium_OpenCart/Pages/Body/RegisterPage/RegisterPage.cs
@@ -12,44 +12,132 @@ namespace TestSite.Pages.RegisterPage
 {
     public class RegisterPage
     {
-        private IWebDriver driver;
+        protected IWebDriver driver;
 
-        public IWebElement LebleRegister
-        { get { return driver.FindElement(By.XPath("//div[contains(@id, 'content') and contains(//h1, 'Register Account')]")); } }
 
-        private IWebElement FirstNameField
-        { get { return driver.FindElement(By.Id("input-firstname")); } }
-        private IWebElement LastNameField
-        { get { return driver.FindElement(By.Id("input-lastname")); } }
-        private IWebElement EMailField
-        { get { return driver.FindElement(By.Id("input-email")); } }
-        private IWebElement TelephoneField
-        { get { return driver.FindElement(By.Id("input-telephone")); } }
-        private IWebElement FaxField
-        { get { return driver.FindElement(By.Id("input-fax")); } }
-        private IWebElement CompanyField
-        { get { return driver.FindElement(By.Id("input-company")); } }
-        private IWebElement Address1Field
-        { get { return driver.FindElement(By.Id("input-address-1")); } }
-        private IWebElement Address2Field
-        { get { return driver.FindElement(By.Id("input-address-2")); } }
-        private IWebElement CityField
-        { get { return driver.FindElement(By.Id("input-city")); } }
-        private IWebElement PostCodeField
-        { get { return driver.FindElement(By.Id("input-postcode")); } }
-        private IWebElement CountryField
-        { get { return driver.FindElement(By.Id("input-country")); } }
-        private IWebElement RegionField
-        { get { return driver.FindElement(By.Id("input-zone")); } }
-        private IWebElement PasswordField
-        { get { return driver.FindElement(By.Id("input-password")); } }
-        private IWebElement PasswordConfirmField
-        { get { return driver.FindElement(By.Id("input-confirm")); } }
-        private bool NewsletterSubscribe { get; set; } = false;
-        private IWebElement PrivacyPolicy
-        { get { return driver.FindElement(By.XPath(".//div[@class='pull-right']//input[@type='checkbox' and @name='agree']")); } }
-        private IWebElement ButtonContinue
-        { get { return driver.FindElement(By.CssSelector("a.btn.btn-primary")); } }
+
+        public IWebElement FirstNameField
+        {
+            get
+            {
+                return driver.FindElement(By.Id("input-firstname"));
+            }
+        }
+        public IWebElement LastNameField
+        {
+            get
+            {
+                return driver.FindElement(By.Id("input-lastname"));
+            }
+        }
+        public IWebElement EMailField
+        {
+            get
+            {
+                return driver.FindElement(By.Id("input-email"));
+            }
+        }
+        public IWebElement TelephoneField
+        {
+            get
+            {
+                return driver.FindElement(By.Id("input-telephone"));
+            }
+        }
+        #region
+        //public IWebElement FaxField
+        //{
+        //    get
+        //    {
+        //        return driver.FindElement(By.Id("input-fax"));
+        //    }
+        //}
+        //public IWebElement CompanyField
+        //{
+        //    get
+        //    {
+        //        return driver.FindElement(By.Id("input-company"));
+        //    }
+        //}
+        //public IWebElement Address1Field
+        //{
+        //    get
+        //    {
+        //        return driver.FindElement(By.Id("input-address-1"));
+        //    }
+        //}
+        //public IWebElement Address2Field
+        //{
+        //    get
+        //    {
+        //        return driver.FindElement(By.Id("input-address-2"));
+        //    }
+        //}
+        //public IWebElement CityField
+        //{
+        //    get
+        //    {
+        //        return driver.FindElement(By.Id("input-city"));
+        //    }
+        //}
+        //public IWebElement PostCodeField
+        //{
+        //    get
+        //    {
+        //        return driver.FindElement(By.Id("input-postcode"));
+        //    }
+        //}
+        //public SelectElement CountryField
+        //{
+        //    get
+        //    {
+        //        return  new SelectElement(driver.FindElement(By.Id("input-country")));
+        //    }
+        //}
+        //public IWebElement RegionField
+        //{
+        //    get
+        //    {
+        //        return driver.FindElement(By.Id("input-zone"));
+        //    }
+        //}
+        #endregion
+        public IWebElement PasswordField
+        {
+            get
+            {
+                return driver.FindElement(By.Id("input-password"));
+            }
+        }
+        public IWebElement PasswordConfirmField
+        {
+            get
+            {
+                return driver.FindElement(By.Id("input-confirm"));
+            }
+        }
+        public bool NewsletterSubscribe { get; private set; } = false;
+        public IWebElement PrivacyPolicy
+        {
+            get
+            {
+                return driver.FindElement(By.XPath(".//div[@class='pull-right']//input[@type='checkbox' and @name='agree']"));
+            }
+        }
+        public IWebElement ButtonContinue
+        {
+            get
+            {
+                return driver.FindElement(By.CssSelector("input.btn.btn-primary"));
+            }
+        }
+        public IWebElement Buttonsuccess
+        {
+            get
+            {
+                return driver.FindElement(By.CssSelector("a.btn.btn-primary"));
+            }
+        }
 
         public RegisterPage(IWebDriver driver)
         {
@@ -88,71 +176,72 @@ namespace TestSite.Pages.RegisterPage
         {
             TelephoneField.SendKeys(telephone);
         }
-        public void ClickFax()
-        {
-            FaxField.Click();
-        }
-        public void InputFax(string fax)
-        {
-            FaxField.SendKeys(fax);
-        }
-        public void ClickCompany()
-        {
-            CompanyField.Click();
-        }
-        public void InputCompanyName(string companyName)
-        {
-            CompanyField.SendKeys(companyName);
-        }
-        public void ClickAddress1()
-        {
-            Address1Field.Click();
-        }
-        public void InputAddress1(string address1)
-        {
-            Address1Field.SendKeys(address1);
-        }
-        public void ClickAddress2()
-        {
-            Address2Field.Click();
-        }
-        public void InputAddress2(string address2)
-        {
-            Address2Field.SendKeys(address2);
-        }
-        public void ClickCity()
-        {
-            CityField.Click();
-        }
-        public void InputCity(string city)
-        {
-            CityField.SendKeys(city);
-        }
-        public void ClickPostCode()
-        {
-            PostCodeField.Click();
-        }
-        public void InputPostCode(string postCode)
-        {
-            PostCodeField.SendKeys(postCode);
-        }
-        public void ClickCountry()
-        {
-            CountryField.Click();
-        }
-        public void InputCountry(string country)
-        {
-            CountryField.SendKeys(country);
-        }
-        public void CkickRegion()
-        {
-            RegionField.Click();
-        }
-        public void InputRegion(string region)
-        {
-            RegionField.SendKeys(region);
-        }
-
+        #region
+        //public void ClickFax()
+        //{
+        //    FaxField.Click();
+        //}
+        //public void InputFax(string fax)
+        //{
+        //    FaxField.SendKeys(fax);
+        //}
+        //public void ClickCompany()
+        //{
+        //    CompanyField.Click();
+        //}
+        //public void InputCompanyName(string companyName)
+        //{
+        //    CompanyField.SendKeys(companyName);
+        //}
+        //public void ClickAddress1()
+        //{
+        //    Address1Field.Click();
+        //}
+        //public void InputAddress1(string address1)
+        //{
+        //    Address1Field.SendKeys(address1);
+        //}
+        //public void ClickAddress2()
+        //{
+        //    Address2Field.Click();
+        //}
+        //public void InputAddress2(string address2)
+        //{
+        //    Address2Field.SendKeys(address2);
+        //}
+        //public void ClickCity()
+        //{
+        //    CityField.Click();
+        //}
+        //public void InputCity(string city)
+        //{
+        //    CityField.SendKeys(city);
+        //}
+        //public void ClickPostCode()
+        //{
+        //    PostCodeField.Click();
+        //}
+        //public void InputPostCode(string postCode)
+        //{
+        //    PostCodeField.SendKeys(postCode);
+        //}
+        ////public void ClickCountry()
+        ////{
+        ////    CountryField.Click();
+        ////}
+        ////public void InputCountry(string country)
+        ////{
+        ////    CountryField.SelectByText(country);
+        ////}
+        //public void CkickRegion()
+        //{
+        //    RegionField.Click();
+        //}
+        //public void InputRegion(string region)
+        //{
+        //    RegionField.SendKeys(region);
+        //}
+        #endregion
         public void ClickPassword()
         {
             PasswordField.Click();
@@ -161,6 +250,7 @@ namespace TestSite.Pages.RegisterPage
         {
             PasswordField.SendKeys(password);
         }
+        
         public void ClickPasswordConfirm()
         {
             PasswordConfirmField.Click();
@@ -169,7 +259,6 @@ namespace TestSite.Pages.RegisterPage
         {
             PasswordConfirmField.SendKeys(passwordConfirm);
         }
-
         public void CheckNewsletterSubscribe()
         {
             Actions action = new Actions(driver);
@@ -180,6 +269,73 @@ namespace TestSite.Pages.RegisterPage
         {
             PrivacyPolicy.Click();
         }
-    }
+        public void ClickButtonContinue()
+        {
+            ButtonContinue.Click();
+        }
+
+        public void ClickInputFirstNameField(string firstName)
+        {
+            FirstNameField.Click();
+            FirstNameField.SendKeys(firstName);
+        }
+        public void ClickInputLastNameField(string lastName)
+        {
+            LastNameField.Click();
+            LastNameField.SendKeys(lastName);
+        }
+        public void ClickInputEMailField(string email)
+        {
+            EMailField.Click();
+            EMailField.SendKeys(email);
+        }
+        public void ClickInputTelephoneField(string telephone)
+        {
+            TelephoneField.Click();
+            TelephoneField.SendKeys(telephone);
+        }
+        public void ClickInputPasswordField(string password)
+        {
+            PasswordField.Click();
+            PasswordField.SendKeys(password);
+        }
+        public void ClickInputPasswordConfirmField(string passwordConfirm)
+        {
+            PasswordConfirmField.Click();
+            PasswordConfirmField.SendKeys(passwordConfirm);
+        }
+
+        public void ClickButtonsuccess()
+        {
+            Buttonsuccess.Click();
+        }
+
+        static bool VerifyRegistrationPage(IWebDriver driver)
+        {
+
+            try
+            {
+                driver.FindElement(By.XPath("//div[contains(@id, 'content') and contains(//h1, 'Register Account')]"));
+                return true;
+            }
+            catch (NoSuchElementException)
+            {
+                return false;
+            }
+
+        }
+        public static RegisterPage UserRegisterPage(IWebDriver driver)
+        {
+            if (VerifyRegistrationPage(driver))
+            {
+                return new RegisterPage(driver);
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }    
 
 }
+
