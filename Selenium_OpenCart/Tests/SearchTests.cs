1f0b210fcf6f77eee64175f045697895c4682a53
lemm1 <mrlolvovgres@gmail.com>
2018-10-29 14:07:13 +0200

    Removed paralizable

--- Selenium_OpenCart/Tests/SearchTests.cs
+++ Selenium_OpenCart/Tests/SearchTests.cs
@@ -8,7 +8,6 @@ using Selenium_OpenCart.Data.Application;
 namespace Selenium_OpenCart.Tests
 {
     [TestFixture]
-    [Parallelizable]
     public class SearchTests
     {
         SearchMethods logicSearch;
