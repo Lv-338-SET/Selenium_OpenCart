e58dfaeedf635b513032084d6ea32c5e406e94e9
Andrii Turkevych <andriiturkevych@gmail.com>
2018-11-02 00:45:07 +0200

    added tests customer and vouchers

--- Selenium_OpenCart/Tests/APITests/VoucherTests.cs
+++ Selenium_OpenCart/Tests/APITests/VoucherTests.cs
@@ -11,42 +11,35 @@ using System.Threading.Tasks;
 namespace Selenium_OpenCart.Tests.APITests
 {
     [TestFixture]
-    class PaymentTests
+    class VoucherTests
     {
         string api_token;
+
         [OneTimeSetUp]
-        public void BeforeClass()
+        public void BeforeAllTests()
         {
             APIMethod api = new APIMethod();
             string key = "d5YFz2RyNjnNXpkTqpNaoGAIPHuipKbmKnlRwOP2Jrls05gZJi3hDNbS8Orvbm5XAYJZ1ckrL3SQqikPo1V7FyPPiG7JEfYhWqjLHhjvXb0HED3EyNt2CHSVLzNIlgpzWzjXFh2HiHfCJd2XSubGlCTczDR5uXP2V5rNX1Gjt8uK05Hd1eeRiytEmoIEDjeXW1mw14oL1qxSBATmmv5CZJzmSTayghm2cXWZYw1msbPEhuItfrBzXJcuaV188neq";
             string username = "Default";
             api_token = (api.ApiGetToken(username, key).Value as ILogin).GetApiToken();
         }
-        [Test]
-        public void TestApiSetPaymentAddress()
-        {
-                APIMethod aPIMethod = new APIMethod();
-                var expected = aPIMethod.ApiSetPaymentAddress("Customer", "Dear", "Somewhere", "KLD", "RUS", "KGD", api_token);
 
-                Assert.AreEqual(expected.Key, HttpStatusCode.OK, "Wrong HTTP code returned");
+        [Test]
+        public void Voucher()
+        {
+            APIMethod aPIMethod = new APIMethod();
+            var expected = aPIMethod.ApiApplyExistingVoucher("Present", api_token);
+
+            Assert.AreEqual(expected.Key, HttpStatusCode.OK);
         }
 
         [Test]
-        public void TestApiGetPaymentMethods()
+        public void VoucherAdd()
         {
             APIMethod aPIMethod = new APIMethod();
-            var expected = aPIMethod.ApiGetPaymentMethods(api_token);
+            var expected = aPIMethod.ApiAddVoucher("Andrii", "tru@tru.com", "Billi", "Billi@bil.com","1", "1234", api_token);
 
-            Assert.AreEqual(expected.Key, HttpStatusCode.OK, "Wrong HTTP code returned");
-        }
-
-        [Test]
-        public void TestApiSetPaymentMethod()
-        {
-            APIMethod aPIMethod = new APIMethod();
-            var expected = aPIMethod.ApiSetPaymentMethod("bank_transfer", api_token);
-
-            Assert.AreEqual(expected.Key, HttpStatusCode.OK, "Wrong HTTP code returned");
+            Assert.AreEqual(expected.Key, HttpStatusCode.OK);
         }
     }
 }
